1e860a0680203156a05d939d2d6e70f796a18ff2
kk4466345 <kk4466345@gmail.com>
2018-09-15 00:13:41 +0530

    adding all files

---
+++ ds/ass2/kuch.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct stack {
+    int top;
+    char arr[]
+}
+
+int main() {
+    int t;
+    char a;
+    scanf("%d%c",&t,&a);
+    while(t--) {
+
+    }
+    return 0;
+}
